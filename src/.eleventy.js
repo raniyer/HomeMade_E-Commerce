189970018dafac40d7807c499dffc5f4f33f99b0
j-devil99 <jaidev.bhui@gmail.com>
2020-08-22 08:12:37 +0530

    added 11ty

---
+++ src/.eleventy.js
@@ -0,0 +1,10 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+    eleventyConfig.addPassthroughCopy('assets')
+    return {
+        passthroughFileCopy: true
+    }
+}
